c6481772ceff6fe868e6ab810f1a218004f26732
dima240-hub <you@example.com>
2025-10-11 13:14:00 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="h-screen w-screen bg-amber-100 text-black flex items-center justify-center">
+			Platforma de Conectare Investitori Startup uri
+		</div>
+	)
+}
+
+export default App
