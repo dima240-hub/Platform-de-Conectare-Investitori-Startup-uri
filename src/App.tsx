93e88424c68bff495caad001f656345b33466f43
dima240-hub <you@example.com>
2025-10-12 14:35:35 +0300

    feat: Shadcn library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 	return (
 		<div className="h-screen w-screen bg-amber-100 text-black flex items-center justify-center">
 			Platforma de Conectare Investitori Startup uri
+			<Button variant="secondary">Get started</Button>
 		</div>
 	)
 }
